9142a81f82df5e44672ebdc229bac69d72c032ce
kinashi <y.nashiki@gmail.com>
2021-02-20 15:32:23 +0900

    update

--- postcss.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  plugins: [
-    require('autoprefixer')({
-      browsers: ['ie >= 11', 'last 3 versions']
-    })
-  ]
-}
--- src/javascripts/app.js
+++
@@ -1,8 +0,0 @@
-import velocity from 'velocity-animate';
-
-window.onload = () => {
-  const header = document.getElementById('header');
-  header.style.opacity = 0
-
-  velocity(header, {opacity: 100}, {duration: 500});
-}
